957f8f142e644ec4ecce6218345e4c6da333f1e1
Amanda Monzon <109624883+MandaMonzon@users.noreply.github.com>
2025-06-21 15:50:14 -0300

    Delete src/IdadeInvalidadeException.java

--- src/IdadeInvalidadeException.java
+++
@@ -1,8 +0,0 @@
-// IdadeInvalidaException.java
-public class IdadeInvalidaException extends Exception {
-
-    // Construtor que aceita uma mensagem de erro
-    public IdadeInvalidaException(String message) {
-        super(message); // Chama o construtor da classe pai (Exception)
-    }
-}
