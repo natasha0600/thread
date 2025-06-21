bf2c08ea2907f0bea418a8059f522f54052e3bcd
Natasha Fernandes <fernandeskelly0600@hotmail.com>
2025-06-20 22:38:17 -0300

    Primeiro commit: implementacao de threads com e sem protecao

--- src/thread/Main.java
+++ src/thread/Main.java
@@ -6,23 +6,17 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
         System.out.println("--- Execução sem proteção ---");
-        Contador contadorSemProtecao = new Contador(); // Novo contador para o teste sem proteção
+        Contador contadorSemProtecao = new Contador();
 
-        // Criando as duas threads para o cenário sem proteção
         MinhaThread thread1SemProtecao = new MinhaThread(contadorSemProtecao, false);
         MinhaThread thread2SemProtecao = new MinhaThread(contadorSemProtecao, false);
 
-        // Iniciando as threads. Elas começam a executar o método 'run'.
         thread1SemProtecao.start();
         thread2SemProtecao.start();
 
-        // Espera as threads terminarem suas execuções.
-        // O 'join()' faz com que a thread principal (main) espere até que a thread 'thread1SemProtecao'
-        // e 'thread2SemProtecao' terminem antes de continuar.
         thread1SemProtecao.join();
         thread2SemProtecao.join();
 
-        // Imprime o resultado final. Esperaríamos 2000, mas será menor devido à condição de corrida.
         System.out.println("Valor final do contador sem proteção: " + contadorSemProtecao.getValor());
         System.out.println("-----------------------------\n");
 
@@ -30,19 +24,15 @@ public class Main {
         System.out.println("--- Execução com proteção (Lock) ---");
         Contador contadorComProtecao = new Contador(); // Novo contador para o teste com proteção
 
-        // Criando as duas threads para o cenário com proteção
         MinhaThread thread1ComProtecao = new MinhaThread(contadorComProtecao, true);
         MinhaThread thread2ComProtecao = new MinhaThread(contadorComProtecao, true);
 
-        // Iniciando as threads.
         thread1ComProtecao.start();
         thread2ComProtecao.start();
 
-        // Espera as threads terminarem suas execuções.
         thread1ComProtecao.join();
         thread2ComProtecao.join();
 
-        // Imprime o resultado final. Desta vez, será 2000 (o valor esperado).
         System.out.println("Valor final do contador com proteção: " + contadorComProtecao.getValor());
         System.out.println("------------------------------------\n");
     }
