28f8e1016f17096c0b3ce7791fc6995e2612081c
Amanda Monzon <109624883+MandaMonzon@users.noreply.github.com>
2025-06-21 15:48:05 -0300

    Update IdadeInvalidaException.java
    New class IdadeInvalida

--- src/excecoes /IdadeInvalidaException.java
+++ src/excecoes /IdadeInvalidaException.java
@@ -1,1 +1,8 @@
+// IdadeInvalidaException.java
+public class IdadeInvalidaException extends Exception {
 
+    // Construtor que aceita uma mensagem de erro
+    public IdadeInvalidaException(String message) {
+        super(message); // Chama o construtor da classe pai (Exception)
+    }
+}
